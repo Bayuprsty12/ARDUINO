2679e220d78653ffce28b1b46888536cd66e5961
Bayuprsty12 <bayuprasetiyo0102@gmail.com>
2023-04-12 12:34:31 +0800

    edit dan menambahkan file

--- Send_data_API_RealTime/Send_data_API_RealTime.ino
+++ Send_data_API_RealTime/Send_data_API_RealTime.ino
@@ -2,6 +2,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <LiquidCrystal_I2C.h>
+#define led D8
 
 //Konfigurasi WiFi
 const char *ssid = "POCO X3 Pro";
@@ -10,15 +12,24 @@ const char *password = "bayu1234";
 //IP Address Server yang terpasang XAMPP
 const char *host = "192.168.225.249";
  
-
 //deklarate Sensor PZEM-004T
 PZEM004Tv30 pzem(14, 12);  // Menggunakan Hardware Serial 3 (RX-D6 -> TX-D5)
+
+//deklarate type lcd i2c
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 unsigned long start, finished, elapsed;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(led, OUTPUT); // deklarasi pin LED 5mm
+
+  lcd.begin();
+  lcd.setCursor(2,0);
+  lcd.print("Welcome User");
+  lcd.setCursor(2,1);
+  lcd.print("Connecting");
+
   WiFi.mode(WIFI_STA);
 
   WiFi.begin(ssid, password);
@@ -42,6 +53,23 @@ void setup() {
 void loop() {
   float current = pzem.current();
   if(current != NAN){
+
+    //tampilan diLCD
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Ready for Using");
+
+    lcd.setCursor(0,1);
+    lcd.print("Current: ");
+    lcd.print(current);
+    lcd.print("amp");
+
+    //fungsi LED 5mm
+    digitalWrite (led, HIGH); // Led Nyala
+    delay(1000);
+
+
+    //tampilan di serial monitor
     Serial.print("Current: ");
     Serial.print(current);
     Serial.println(" A");
@@ -50,35 +78,40 @@ void loop() {
   }
 
   if(current != NAN && current>0){
+    //tampilan diLCD
+    lcd.clear();
+    lcd.setCursor(3,0);
+    lcd.print("ON Using");
+    lcd.setCursor(0,1);
+    lcd.print("Mulai Menghitung"); 
+
     Serial.println("Mulai menghitung....");
     
     float voltage_sensor, current_sensor, power_sensor, energy_sensor;
     voltage_sensor = pzem.voltage();
     current_sensor = pzem.current();
-    power_sensor = pzem.power();
+    power_sensor = voltage_sensor * current_sensor;
     energy_sensor = pzem.energy();
-    /*
-    status pembacaan :
-    1 -> start
-    2 -> on progress
-    3 -> stop
-    */
-
+    
     int status = 1;
     //mulai mengirim data
     send_data(status, current_sensor, voltage_sensor, power_sensor, energy_sensor);
     delay(2000);
-
+    
     while(pzem.current()>0){
       status = 2;
       send_data(status, current_sensor, voltage_sensor, power_sensor, energy_sensor);
-      delay(5000);
+        //fungsi LED 5mm
+        digitalWrite (led, HIGH); // Led Nyala
+        delay(500);
+
+      digitalWrite (led, LOW); // Led Mati
+      delay(30000); //send data ke server 5 detik sekali
     }
 
     //selesai perhitungan
     status = 3;
     send_data(status, current_sensor, voltage_sensor, power_sensor, energy_sensor);
-
   }
 
   delay(1000);
